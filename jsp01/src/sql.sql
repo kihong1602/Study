08f6fc26b7a65d571a411e3c98a5566307bd6181
Kihong <kks4517@naver.com>
2023-09-18 23:21:05 +0900

    create table user

--- jsp01/src/sql.sql
+++ jsp01/src/sql.sql
@@ -1,1 +1,10 @@
-drop table
+create table user
+(
+    no             int(5) auto_increment primary key,
+    id             varchar(30) not null unique,
+    password       varchar(50) not null,
+    postcode       int(8)      not null,
+    address        varchar(100),
+    address_detail varchar(100),
+    reg_date       timestamp default current_timestamp on update current_timestamp
+);
